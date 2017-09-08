72391c34dafdbf5bc4f2786995d84e0acf9202cc
ryanafrish <ryanafrish7@gmail.com>
2017-09-08 11:53:47 +0000

    Gradient + SIFT + STASM

--- orch.py
+++ orch.py
@@ -1,11 +1,29 @@
 import cv2
 import phase_zero
+import stasm as asm
 
+print(asm.search_single)
+
+# I = cv2.imread("./data/00001.jpg", cv2.IMREAD_GRAYSCALE)
 I = cv2.imread("./data/sketch-small.png", cv2.IMREAD_GRAYSCALE)
 
-I = phase_zero.align_face(I)
-phase_zero.compute_gradient(I)
+cv2.imshow('Original', I)
 
-cv2.imshow('image', I)
+I = phase_zero.align_face(I)
+
+I1 = cv2.medianBlur(I, 5)
+cv2.imshow('image median', I1)
+I2 = cv2.bilateralFilter(I, 4, 75, 75)
+cv2.imshow('image bilateral', I2)
+
+sift = cv2.xfeatures2d.SIFT_create()
+kp = sift.detect(I,None)
+img=cv2.drawKeypoints(I, kp, I)
+
+cv2.imshow('Image SIFT', img)
+
+phase_zero.compute_gradient(I2)
+cv2.imshow('Message', phase_zero.apply_stasm(I))
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
--- phase_zero.py
+++ phase_zero.py
@@ -1,6 +1,9 @@
 import cv2
+import stasm as asm
 import math
 import numpy as np
+
+np.set_printoptions(threshold=np.nan)
 
 # Face alignment using eye detection
 # Refer to: https://stackoverflow.com/questions/39296461/how-to-straighten-a-tilted-face-after-cropping
@@ -26,6 +29,8 @@ def align_face(img):
     # shift the positions to complete image space
     eyeR_x += math.floor(img_w / 2)
 
+    #todo: for better detection, don't divide by two
+
     # cv2.rectangle(I, (eyeL_x, eyeL_y), (eyeL_x + eyeL_w, eyeL_y + eyeL_h), 255, 2)
     # cv2.rectangle(I, (eyeR_x, eyeR_y), (eyeR_x + eyeR_w, eyeR_y + eyeR_h), 255, 2)
 
@@ -38,28 +43,66 @@ def align_face(img):
 # Gradient of the Image
 
 def compute_gradient(img):
-    sobel_x = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize = 7)
-    sobel_y = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize = 7)
+    sobel_x = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=3)
+    sobel_y = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=3)
 
-    angle = np.rad2deg(np.arctan(np.divide(sobel_y, sobel_x)))
+    angle = np.add(np.rad2deg(np.arctan(np.divide(sobel_y, sobel_x))), 180)
+    magnitude2 = np.multiply(np.add(sobel_x, sobel_y), 0.5)
     magnitude = np.sqrt(np.square(sobel_x) + np.square(sobel_y))
 
     s = np.ones(img.shape) * 255
-    v = np.ones(img.shape) * 255
 
-    hsv_image = np.uint8(cv2.merge([angle, s, v]))
+    hsv_image = np.uint8(cv2.merge([angle, s, magnitude]))
+    hsv_image2 = np.uint8(cv2.merge([angle, s, magnitude2]))
 
     out = cv2.cvtColor(hsv_image, cv2.COLOR_HSV2BGR)
+    out2 = cv2.cvtColor(hsv_image2, cv2.COLOR_HSV2BGR)
 
-    cv2.imshow('Gra', out)
-    cv2.imshow('Gras', magnitude)
+    cv2.imshow('Gradient Angle Hue', out)
+    apply_stasm(cv2.cvtColor(out, cv2.COLOR_BGR2GRAY))
+    cv2.imshow('Gradient Angle Hue 2', out2)
+    cv2.imshow('Gradient Magnitude', magnitude2)
 
-    print(angle)
     print(magnitude)
+
+    # sobel_x = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize = 3)
+    # sobel_y = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize = 3)
+
+    # angle = np.rad2deg(np.arctan(np.divide(sobel_y, sobel_x)))
+    # magnitude = np.sqrt(np.square(sobel_x) + np.square(sobel_y))
+    #
+    # s = np.ones(img.shape) * 255
+    # v = np.ones(img.shape) * 255
+    #
+    # hsv_image = np.uint8(cv2.merge([angle, s, v]))
+    #
+    # out = cv2.cvtColor(hsv_image, cv2.COLOR_HSV2BGR)
+    #
+    # cv2.imshow('Gra', out)
+    # cv2.imshow('Gras', magnitude)
+    #
+    # print(angle)
+    # print(magnitude)
+    #
+    # img_phase = cv2.phase(sobel_x, sobel_y, angleInDegrees=True)
+    # img_phase = np.multiply(img_phase, 255/360)
+    # print(img_phase)
+
 
     cv2.imshow('Grad x', sobel_x)
     cv2.imshow('Grad y', sobel_y)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
+    # cv2.imshow('Grad phase', img_phase)
     return
+
+def apply_stasm(img):
+    landmarks = asm.search_single(img)
+    if len(landmarks) == 0:
+        print("No face found in the image")
+    else:
+        landmarks = asm.force_points_into_image(landmarks, img)
+        landmarks = np.uint8(landmarks)
+        for point in landmarks:
+            # img[round(point[1])][round(point[0])] = 255
+            cv2.circle(img, (point[0], point[1]), 3, 255, -1)
+
+    return img
